1a208fef7a685834daec2912a697ee78252dca9f
Kody Yang <zryang@uwaterloo.ca>
2016-11-05 06:22:15 -0400

    added house function

--- kody.py
+++ kody.py
@@ -1,6 +1,8 @@
 import Leap, sys, thread, time
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 
+lastWord = ""
+
 def please(frame):
         for hand in frame.hands:
             for gesture in frame.gestures():
@@ -24,8 +26,33 @@ def please(frame):
                                     if hand.direction.yaw * Leap.RAD_TO_DEG >=-115 and hand.direction.yaw * Leap.RAD_TO_DEG <= -65 :
                                         if circle.progress >= 1.75:
                                             print "please"
-                                            #lastWord = "please"
+                                            lastWord = "please"
                                             return
+
+def house(frame):
+    house = [False, False]
+
+    for hand in frame.hands:
+
+        normal = hand.palm_normal
+        direction = hand.direction
+
+        if hand.is_left:
+            if (100 >= (direction.yaw * Leap.RAD_TO_DEG) >= 60) and (50 >= (normal.roll * Leap.RAD_TO_DEG) >= 20):
+                house[0] = True
+            else:
+                house[0] = False
+        elif hand.is_right:
+            if (-100 <= direction.yaw * Leap.RAD_TO_DEG <= -60) and (-50 <= normal.roll * Leap.RAD_TO_DEG <= -20):
+                house[1] = True
+            else:
+                house[1] = False
+
+        time.sleep(0.3)
+
+        if house[0] and house[1]:
+            print 'house'
+            lastWord = 'house'
 
 class LeapMotionListener(Leap.Listener):
     finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
@@ -54,8 +81,13 @@ class LeapMotionListener(Leap.Listener):
 
 
     def on_frame(self, controller):
+        print lastWord
         frame = controller.frame()
-        please(frame)
+        if lastWord != "please":
+            please(frame)
+
+        if lastWord != "house":
+            house(frame)
         #lastWord = ""
         #frame = controller.frame()
 
